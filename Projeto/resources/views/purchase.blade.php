9bef0f37844a0de0ff221f3050b2a0e703f0abf5
lucas42380 <42380@ufp.edu.pt>
2024-01-12 12:24:29 +0000

    purshase bug fix

--- Projeto/resources/views/purchase.blade.php
+++ Projeto/resources/views/purchase.blade.php
@@ -10,19 +10,22 @@
         <div class="activity-info">
             <h3>{{ $atividade->nome }}</h3>
             <p>{{ $atividade->descricao }}</p>
-            <p>Data e Horário: {{ $atividade->data_horario }}</p>
             <p>Preço por Bilhete: ${{ $atividade->preco }}</p>
         </div>
 
         <!-- Formulario de compra -->
-        <form action="/carrinho/adicionar" method="post" class="purchase-form">
+        <form action="/pagamento/{{$atividade->id}}" method="post" class="purchase-form">
             @csrf
             <input type="hidden" name="atividade_id" value="{{ $atividade->id }}">
+            <select name="agendamento" id="agendamento">
+                <option value="1">serfef</option>
+                <option value="2">erverveer</option>
+            </select>
             <label for="quantidade" class="form-label" style="color: white;">Quantidade de Bilhetes:</label>
             <input type="number" id="quantidade" name="quantidade" class="form-input" min="1" value="1" required>
             <br>
             <input type="hidden" name="atividade_id" value="{{ $atividade->id }}">
-            <button type="submit" class="btn-add-to-cart">Adicionar ao Carrinho</button>
+            <button type="submit" class="adicionar-carrinho-btn">Comprar</button>
         </form>
     </div>
 @endsection
